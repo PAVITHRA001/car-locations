01ad4b1e7af7917cf4fa8fc5d4954c5e63e268da
pr3 <pavithra.r@mrcooper.com>
2019-07-15 03:18:05 +0530

    Added api to constants

--- src/sagas/car_location_fetch_saga.js
+++ src/sagas/car_location_fetch_saga.js
@@ -1,10 +1,11 @@
 import { call, put } from 'redux-saga/effects';
 import * as actions from '../actions/action.js';
 import callFetchApi from '../fetch_api.js';
+import * as constants from '../constants';
 
 export default function* carLocationsFetchSaga(action) {
     try {
-        const api_url = 'https://challenge.smove.sg/locations';
+        const api_url = constants.carLocationsAPI;
         const response = yield call(callFetchApi, api_url, {}, 'GET');
 
         yield put({
